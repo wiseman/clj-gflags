8f954933a06bff14fae6a871460da6c3c4694294
John Wiseman <jwiseman@morseproject.com>
2013-11-08 15:45:37 -0800

    Added make-flag-values and cleaned up issues around using something other than the default *flags*.  Added test for string flag default value.

--- src/com/lemonodor/gflags.clj
+++ src/com/lemonodor/gflags.clj
@@ -45,13 +45,15 @@
     (:__flags this)))
 
 
-(def ^:dynamic *flags* (atom (->FlagValues {} {})))
+(defn make-flag-values []
+  (atom (->FlagValues {} {})))
 
 
-(defn flag-values
-  ([values] (into {}
-                  (for [[name flag] (:__flags @values)] [name flag])))
-  ([] (flag-values *flags*)))
+(def ^:dynamic *flags* (make-flag-values))
+
+
+(defn flag-values []
+  (into {} (for [[name flag] (:__flags @*flags*)] [name flag])))
 
 
 (defn flag-name-to-symbol [name]
@@ -61,10 +63,12 @@
 ;; Equivalent of gflags.py FLAGS.  FIXME: Should be able to return a
 ;; thing that acts like a map but just indexes into existing flag
 ;; values without actually creating a map, right?
-(defn flags
-  ([values] (into {}
-                  (for [[name flag] (:__flags @values)] [(flag-name-to-symbol name) (:value @flag)])))
-  ([] (flags *flags*)))
+(defn flags []
+  (into
+   {}
+   (for [[name flag] (:__flags @*flags*)]
+     [(flag-name-to-symbol name) (:value @flag)])))
+
 
 (defrecord Flag
     [name
@@ -86,27 +90,25 @@
   value)
 
 
-(defn define-flag [flagvalues flag ns]
-  (swap! flagvalues register-flag flag ns))
+(defn define-flag [flag ns]
+  (swap! *flags* register-flag flag ns))
 
 
-(defn define [parser name default help serializer & args]
+(defn define [parser name default help & args]
   (define-flag
-    *flags*
     (map->Flag
      (merge
       {:help "(no help available)"}
       {:parser parser
        :name name
        :default default
-       :help help
-       :serializer serializer}
+       :help help}
       (apply hash-map args)))
     nil))
 
 
 (defn define-string [name default help & args]
-  (apply define string-parser name default help string-serializer args))
+  (apply define string-parser name default help :serializer string-serializer args))
 
 
 (defn boolean-parser [argument]
@@ -125,17 +127,15 @@
          args))
 
 
-(defn parse-flags
-  ([argv] (parse-flags argv *flags*))
-  ([argv flagvalues]
-     (let [args (rest argv)
-           flags (flag-map @flagvalues)
-           longopts (for [[name _] flags] (str name "="))
-           shortopts (apply str
-                            (for [[name flag] flags :when (= (count name) 1)]
-                              (if (:boolean flag)
-                                name
-                                (str name ":"))))
-           [optlist, unparsed-args] (getopt/getopt args shortopts longopts)]
-       (swap! flagvalues update-flag-values optlist)
-       unparsed-args)))
+(defn parse-flags [argv]
+  (let [args (rest argv)
+        flags (flag-map @*flags*)
+        longopts (for [[name _] flags] (str name "="))
+        shortopts (string/join
+                   (for [[name flag] flags :when (= (count name) 1)]
+                     (if (:boolean flag)
+                       name
+                       (str name ":"))))
+        [optlist, unparsed-args] (getopt/getopt args shortopts longopts)]
+    (swap! *flags* update-flag-values optlist)
+    unparsed-args))
--- test/com/lemonodor/gflags_test.clj
+++ test/com/lemonodor/gflags_test.clj
@@ -3,18 +3,31 @@
             ;;[clojure.pprint :as pprint]
             [com.lemonodor.gflags :as gflags]))
 
+
 (deftest string-test
-  (testing "Oh boy"
-    (gflags/define-string "filename"
-      "default-filename"
-      "The input filename"
-      :short-name "f")
-    (let [args ["argv0" "--filename" "foo" "arg1"]
-          unparsed-args (gflags/parse-flags args)]
-      ;;(pprint/pprint (gflags/flag-values))
-      (is (= unparsed-args ["arg1"]))
-      (let [flags (gflags/flags)]
-        ;;(pprint/pprint flags)
-        (is (contains? flags :filename))
-        (is (not (contains? flags :unknown-flag)))
-        (is (= (flags :filename) "foo"))))))
+  (testing "string flag"
+    (binding [gflags/*flags* (gflags/make-flag-values)]
+      (gflags/define-string "filename"
+        "default-filename"
+        "The input filename"
+        :short-name "f")
+      (let [args ["argv0" "--filename" "foo" "arg1"]
+            unparsed-args (gflags/parse-flags args)]
+        (is (= unparsed-args ["arg1"]))
+        (let [flags (gflags/flags)]
+          (is (contains? flags :filename))
+          (is (not (contains? flags :unknown-flag)))
+          (is (= (flags :filename) "foo"))))))
+  (testing "string flag default value"
+    (binding [gflags/*flags* (gflags/make-flag-values)]
+      (gflags/define-string "filename"
+        "default-filename"
+        "The input filename"
+        :short-name "f")
+      (let [args ["argv0" "arg1"]
+            unparsed-args (gflags/parse-flags args)]
+        (is (= unparsed-args ["arg1"]))
+        (let [flags (gflags/flags)]
+          (is (contains? flags :filename))
+          (is (not (contains? flags :unknown-flag)))
+          (is (= (flags :filename) "default-filename")))))))
