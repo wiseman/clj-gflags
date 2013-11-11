64d07426f1e78ce276d2e590b7e2a420b3ae8651
John Wiseman <jwiseman@morseproject.com>
2013-11-11 13:17:26 -0800

    Boolean flags kinda work.

--- src/com/lemonodor/gflags.clj
+++ src/com/lemonodor/gflags.clj
@@ -14,13 +14,17 @@
 (defprotocol FlagValuesProtocol
   (register-flag [this flag ns])
   (update-flag-values [this optlist])
-  (flag-map [this]))
+  (flag-map [this])
+  (all-flags [this]))
 
 
-
-(defn flag-name [optname]
+(defn optname-to-flag-name [optname]
   (let [[_ name] (re-matches #"--?(.+)" optname)]
     name))
+
+
+(defn flag-names [flag]
+  (filter identity [(:name flag) (:short-name flag)]))
 
 
 (defrecord FlagValues [__flags __flags_by_ns]
@@ -28,21 +32,30 @@
   (register-flag [this flag ns]
     ;; We set the flag's value to be its default here (but it's still
     ;; not considered present).
-    (let [flag (atom (assoc flag :value (:default flag)))
+    (let [flag-atom (atom (assoc flag :value (:default flag)))
           flags-by-ns (:__flags_by_ns this)
-          name-entries (if (:short-name @flag)
-                         {(:short-name @flag) flag
-                          (:name @flag) flag}
-                         {(:name @flag) flag})]
+          name-entries (into {} (for [name (flag-names flag)] [name flag-atom]))]
+      ;; Check that a flag with this name isn't already registered.
+      (when-let [conflicting-flag (some (:__flags this) (flag-names flag))]
+        (throw (Exception.
+                (str "Flag named \"" (string/join
+                                      "\" and \"" (flag-names flag)) "\""
+                     " defined in " (:namespace flag)
+                     " conflicts with flag named \""
+                     (string/join
+                      "\" and \"" (flag-names @conflicting-flag)) "\""
+                     " defined in " (:namespace @conflicting-flag)))))
       (->FlagValues (merge (:__flags this) name-entries)
-                    (assoc flags-by-ns ns (conj (flags-by-ns ns) flag)))))
+                    (assoc flags-by-ns ns (conj (flags-by-ns ns) flag-atom)))))
   (update-flag-values [this optlist]
     (doseq [[optname value-string] optlist]
-      (let [name (flag-name optname)]
+      (let [name (optname-to-flag-name optname)]
         (swap! ((:__flags this) name) set-flag-value value-string)))
     this)
   (flag-map [this]
-    (:__flags this)))
+    (:__flags this))
+  (all-flags [this]
+    (distinct (for [[name flag-atom] (:__flags this)] @flag-atom))))
 
 
 (defn make-flag-values []
@@ -78,6 +91,7 @@
      short-name
      boolean
      present
+     namespace
      parser
      serializer
      allow-override])
@@ -102,9 +116,10 @@
       {:parser parser
        :name name
        :default default
-       :help help}
+       :help help
+       :namespace *ns*}
       (apply hash-map args)))
-    nil))
+    *ns*))
 
 
 (defn define-string [name default help & args]
@@ -121,10 +136,26 @@
 
 
 (defn define-boolean [name default help & args]
-  (apply define boolean-parser name default
-         (or help "a boolean value")
-         nil
+  (apply define boolean-parser name default (or help "a boolean value")
+         :boolean true
          args))
+
+
+(defn- full-replacements-for-boolean-flag [flag]
+  (let [replacements (reduce into {}
+                             (map #(-> [[(str "--" %) (str "--" % "=true")]
+                                        [(str "--no" %) (str "--" % "=false")]])
+                                  (flag-names flag)))]
+    replacements))
+
+
+(defn cook-boolean-args [flags args]
+  (let [boolean-flags (filter #(:boolean %) flags)
+        replacements (reduce
+                      into {} (map full-replacements-for-boolean-flag
+                                   boolean-flags))
+        cooked-args (map #(get replacements % %) args)]
+    cooked-args))
 
 
 (defn parse-flags [argv]
@@ -136,6 +167,14 @@
                      (if (:boolean flag)
                        name
                        (str name ":"))))
+        ;; Correct the argv to support the google style of passing
+        ;; boolean parameters.  Boolean parameters may be passed by
+        ;; using --mybool, --nomybool, --mybool=(true|false|1|0).
+        ;; getopt does not support having options that may or may not
+        ;; have a parameter.  We replace instances of the short form
+        ;; --mybool and --nomybool with their full forms:
+        ;; --mybool=(true|false).
+        args (cook-boolean-args (all-flags @*flags*) args)
         [optlist, unparsed-args] (getopt/getopt args shortopts longopts)]
     (swap! *flags* update-flag-values optlist)
     unparsed-args))
--- test/com/lemonodor/gflags_test.clj
+++ test/com/lemonodor/gflags_test.clj
@@ -31,3 +31,38 @@
           (is (contains? flags :filename))
           (is (not (contains? flags :unknown-flag)))
           (is (= (flags :filename) "default-filename")))))))
+
+
+(deftest boolean-test
+  (testing "boolean flag"
+    (binding [gflags/*flags* (gflags/make-flag-values)]
+      (gflags/define-boolean "enable-unicorns"
+        false
+        "Whether we should enable unicorns"
+        :short-name "u")
+      (let [args ["argv0" "--enable-unicorns" "arg1"]
+            unparsed-args (gflags/parse-flags args)]
+        (is (= unparsed-args ["arg1"]))
+        (let [flags (gflags/flags)]
+          (is (contains? flags :enable-unicorns))
+          (is (not (contains? flags :unknown-flag)))
+          (is (flags :enable-unicorns)))))))
+
+
+(deftest redefinition-test
+  (testing "colliding definitions throw errors"
+    (let [ns1 (create-ns 'com.lemonodor.gflags-test.ns1)
+          ns2 (create-ns 'com.lemonodor.gflags-test.ns2)]
+    (binding [gflags/*flags* (gflags/make-flag-values)]
+      (binding [*ns* ns1]
+        (gflags/define-string "filename"
+          "default-filename"
+          "The input filename"
+          :short-name "f"))
+      (is (thrown-with-msg?
+           Exception #"flag.*conflicts"
+           (binding [*ns* ns2]
+             (gflags/define-string "filename"
+               "default-filename"
+               "The input filename"
+               :short-name "f"))))))))
