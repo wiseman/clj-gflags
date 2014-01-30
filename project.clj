f39c5c71788a1c5d75b8a7b2d61aa51aad57f849
John Wiseman <jwiseman@morseproject.com>
2014-01-30 15:29:01 -0800

    Bumped version to 0.2.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemonodor/gflags "0.1.2-SNAPSHOT"
+(defproject com.lemonodor/gflags "0.2.0"
   :description "Google flags/gflags for clojure."
   :url "http://github.com/wiseman/clj-gflags/"
   :license {:name "MIT License"
