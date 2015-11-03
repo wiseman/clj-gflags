d868975a925e555b9e1c1b7f95040e059122cd87
John Wiseman <jjwiseman@gmail.com>
2015-11-02 23:16:12 -0800

    Version 0.7.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.lemonodor/gflags "0.7.3"
+(defproject com.lemonodor/gflags "0.7.4-SNAPSHOT"
   :description "Google flags/gflags for clojure."
   :url "http://github.com/wiseman/clj-gflags/"
   :license {:name "MIT License"
